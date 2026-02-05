955b509adb9c89981f86ef460e378d7d3811227b
Xna28 <XanderGalusha4@gmail.com>
2026-02-04 20:46:38 -0600

    feat(backend) - modified and added to get APIs

--- Database.py
+++ Database.py
@@ -2,9 +2,7 @@ import mysql.connector
 from mysql.connector.connection import MySQLConnection
 
 
-    
-
-class Database():
+class DatabaseObject():
 
     def __init__(self, host, port, userName, passWord, database):
         self.port = port
@@ -29,27 +27,84 @@ class Database():
             self.__mySQLConnectionInstance = currentDatabaseInstance
         except mysql.connector.Error as error:
             print("could not connect")
+
+
+    def GetRooms(self, roomId: int):
+
+        try:
+            cursor = self.__mySQLConnectionInstance.cursor(dictionary=True)
+
+            if(roomId != None):
+                query = f'''
+                SELECT * from room r where r.room_id  = %s
+                '''
+                cursor.execute(query, (roomId,))
+
+            else:
+                query = 'SELECT * from room r'
+                cursor.execute(query)
+
+            rooms = cursor.fetchall()
+            cursor.close()
+
+            return rooms
+        except mysql.connector.Error as error:
+            return error
+
+    
     
     def GetReservations(self):
-        cursor = self.__mySQLConnectionInstance.cursor(dictionary=True)
+        try:
+            cursor = self.__mySQLConnectionInstance.cursor(dictionary=True)
 
-        query = '''
-SELECT
-    a.reservation_id,
-    b.title, description, requirement, numberParticipant, location, startTime, endTime,
-    c.name, email, role
-FROM reservation a
-	inner join event b
-on a.eventId = b.event_id
-	inner join host c
-on a.hostId = c.host_id
+            query = '''
+            SELECT
+                a.reservation_id,
+                b.title, description, requirement, numberParticipant, location, startTime, endTime,
+                c.name, email, role
+            FROM reservation a
+	        inner join event b
+            on a.eventId = b.event_id
+	        inner join host c
+            on a.hostId = c.host_id
+            '''
+
+            cursor.execute(query)
+            reservations = cursor.fetchall()
+            cursor.close()
+
+            return reservations
+        except mysql.connector.Error as error:
+            return error
+    
+    
+    def GetHosts(self, hostId: int):
+        try:
+            cursor = self.__mySQLConnectionInstance.cursor(dictionary=True)
+
+            if(hostId != None):
+                query = f'''
+                SELECT * FROM host h
+                WHERE h.host_id = %s
+                '''
+                cursor.execute(query, (hostId,))
+                hosts = cursor.fetchall()
+            else:
+                query = f'''
+                SELECT * FROM host h
+                '''
+
+                cursor.execute(query)
+                hosts = cursor.fetchall()
+            
+            cursor.close()
+            return hosts
+        except mysql.connector.Error as error:
+            return error
 
 
-        '''
 
-        cursor.execute(query)
-        reservations = cursor.fetchall()
-        cursor.close()
 
-        return reservations
+
+        
 
--- main.py
+++ main.py
@@ -5,8 +5,11 @@ import json
 from fastapi.responses import JSONResponse
 from fastapi.encoders import jsonable_encoder
 from pydantic import BaseModel
-from Database import *
+from Database import DatabaseObject
 import os
+from datetime import datetime
+
+app = FastAPI()
 
 ##These are enviromental variables for the Database. You will need to create your own.
 host = os.getenv('host')
@@ -15,16 +18,42 @@ userName = os.getenv('usernameDB')
 passWord = os.getenv('password')
 database = os.getenv('database')
 
-currentDatabase = Database(host, port, userName, passWord, database)
+currentDatabase = DatabaseObject(host, port, userName, passWord, database)
 currentDatabase.CreateMySQLConnectorInstance()
 
-app = FastAPI()
+@app.get("/rooms")
+async def GetRooms(roomId: int | None = None):
+    roomsJSON = jsonable_encoder(currentDatabase.GetRooms(roomId))
+    return JSONResponse(content=roomsJSON)
+
 
 @app.get("/reservations")
 async def GetEvents():
     eventsJSON = jsonable_encoder(currentDatabase.GetReservations())
     return JSONResponse(content=eventsJSON)
 
+
+@app.get("/hosts")
+async def GetHosts(hostId: int | None = None):
+    hostsJSON = jsonable_encoder(currentDatabase.GetHosts(hostId))
+    return hostsJSON
+
+
+'''
+class ReservationEventModel(BaseModel):
+    hostName: str
+    eventTitle: str
+    eventDescription: str
+    eventRequirements: str
+    eventsGuestCount: int
+    eventLocationId: int
+    eventDateTime: datetime
+
+    
+@app.post("/reservations")
+async def CreateReservationAndEvent(ReservationEvent:ReservationEventModel):
+    print(ReservationEvent.eventDateTime)
+'''
 
 #To run this server
 #1. Verify your terminal is currently referencing this file path (use cd .\Backend\main.py if not).
